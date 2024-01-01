be829e88ab0fb417c683d0db909aa8bd035c9db6
ssmohanty153 <ssmohanty153@gmail.com>
2024-01-01 18:38:06 +0530

    write all the controller and router

--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannalProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUseCoverImage, updateUserAvtar } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { veryfyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
@@ -23,5 +23,23 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(veryfyJWT, logoutUser);
 
 
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/refresh-token").post(refreshAccessToken);
+
+
+router.route("/change-password").post(veryfyJWT, changeCurrentPassword);
+
+router.route("/current-user").get(veryfyJWT, getCurrentUser);
+
+router.route("/update-account").patch(veryfyJWT, updateAccountDetails);
+
+router.route("/avtar").patch(veryfyJWT, upload.single("avatar"), updateUserAvtar);
+
+
+router.route("/cover-image").patch(veryfyJWT, upload.single("coverImage"), updateUseCoverImage);
+
+router.route("/c/:username").get(veryfyJWT, getUserChannalProfile);
+
+router.route("/history").get(veryfyJWT, getWatchHistory);
+
+
 export default router
